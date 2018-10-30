2715a519c575e15b304d2f40fa62f188dbfd410b
tom <tom.guibard45@gmail.com>
2018-10-30 23:20:37 +0100

    Vouallllla

--- src/Controller/CandyController.php
+++ src/Controller/CandyController.php
@@ -15,19 +15,9 @@ class CandyController extends \Controller\AbstractController
     public function index()
     {
         $client = new Client();
-        $a=1;
+        $a = 1;
         if (isset($_GET['button'])) {
-            if ($_GET['button'] == 'previous') {
-                if (($a-1) <= 0) {
-                    $a = 1;
-                } else {
-                    $a--;
-                }
-            } elseif ($_GET['button'] == 'next') {
-                $a++;
-            } else {
-                $a = $_GET['button'];
-            }
+            $a = $_GET['button'];
         }
         $res = $client->request('GET', "https://ssl-api.openfoodfacts.org/country/france/category/candies/$a.json");
         $coucou = json_decode($res->getBody(), TRUE);
