a344e51bdb4c28215aca0647bd93a413084e004c
Vivien Piveteau <vivien.piveteau@hotmail.fr>
2018-10-31 04:32:43 +0100

    resolve conflict candie.php

--- src/Model/Candie.php
+++ src/Model/Candie.php
@@ -14,6 +14,7 @@ class Candie
    private $id;
    private $have_candie;
    private $ean;
+   private $adress;
     
     /**
      * @return mixed
@@ -77,83 +78,5 @@ class Candie
     public function setAdress($adress): void
     {
         $this->adress = $adress;
-    }
-   private $adress;
-}
-=======
- * User: wcs
- * Date: 23/10/17
- * Time: 10:57
- * PHP version 7
- */
-
-namespace Model;
-
-/**
- * Class Item
- *
- */
-class Candie
-{
-    private $id;
-
-    private $adress;
-
-    private $ean;
-
-    /**
-     * @return mixed
-     */
-    public function getEan()
-    {
-        return $this->ean;
-    }
-
-    /**
-     * @param mixed $ean
-     */
-    public function setEan($ean): void
-    {
-        $this->ean = $ean;
-    }
-
-    /**
-     * @return int
-     */
-    public function getId(): int
-    {
-        return $this->id;
-    }
-
-    /**
-     * @param mixed $id
-     *
-     * @return Candie
-     */
-    public function setId($id): Candie
-    {
-        $this->id = $id;
-
-        return $this;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getAdress(): string
-    {
-        return $this->adress;
-    }
-
-    /**
-     * @param mixed $adress
-     *
-     * @return Candie
-     */
-    public function setAdress($adress):Candie
-    {
-        $this->adress = $adress;
-
-        return $this;
     }
 }
